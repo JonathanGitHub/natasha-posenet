561dfbde0b3accf534df84a20a6c7d3f7eb7cd38
balta-z-r <zunigaruizb@gmail.com>
2021-03-21 17:39:12 +0800

    feat: connection to socket

--- opencv_compdemo.py
+++ opencv_compdemo.py
@@ -12,27 +12,52 @@ from pycoral.utils.dataset import read_label_file
 from pycoral.utils.edgetpu import make_interpreter
 from pycoral.utils.edgetpu import run_inference
 
+import socketio
+
 # LOOK TO
 # https://github.com/google-coral/examples-camera/blob/master/opencv/detect.py
 
-# NEXT TIME 
+# NEXT TIME
 # IMPLEMENT THIS ONTO ALIEN INVASION
 # PROJECT STICK CHARACTER ONTO THE IMAGE
 # STICK DATA TO ALIEN INVASION
 
+
 def main():
-    
+
+    # standard Python
+    sio = socketio.Client()
+
+    @sio.event
+    def connect():
+        print("I'm connected!")
+
+    @sio.event
+    def connect_error():
+        print("The connection failed!")
+
+    @sio.event
+    def disconnect():
+        print("I'm disconnected!")
+
+    sio.connect('http://localhost:5000', headers={"type": "python"})
+
     # None of these arguments are needed
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     # parser.add_argument('--mirror', help='flip video horizontally', action='store_true')
     parser.add_argument('--model', help='.tflite model path.', required=False)
     parser.add_argument('--res', help='Resolution', default='640x480',
                         choices=['480x360', '640x480', '1280x720'])
-    parser.add_argument('--videosrc', help='Which video source to use', default='/dev/video0')
-    parser.add_argument('--h264', help='Use video/x-h264 input', action='store_true')
-    parser.add_argument('--jpeg', help='Use image/jpeg input', action='store_true')
-    parser.add_argument('--camera_idx', type=int, help='Index of which video source to use. ', default = 0)
-    
+    parser.add_argument(
+        '--videosrc', help='Which video source to use', default='/dev/video0')
+    parser.add_argument(
+        '--h264', help='Use video/x-h264 input', action='store_true')
+    parser.add_argument(
+        '--jpeg', help='Use image/jpeg input', action='store_true')
+    parser.add_argument('--camera_idx', type=int,
+                        help='Index of which video source to use. ', default=0)
+
     args = parser.parse_args()
 
     default_model = 'models/mobilenet/posenet_mobilenet_v1_075_%d_%d_quant_decoder_edgetpu.tflite'
@@ -63,21 +88,24 @@ def main():
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
-        
+
         # image flip (0: x-axis, 1: y-axis, -1: both axis)
-        # image is flipped on default 
+        # image is flipped on default
         frame = cv2.flip(frame, 1)
         im = Image.fromarray(frame)
         poses, _ = engine.DetectPosesInImage(im)
 
-        print("INFERENCE")
+        # print("INFERENCE")
 
         for pose in poses:
+            data = {}
             # if pose.score < 0.4: continue
-            print('\nPose Score: ', pose.score)
+            # print('\nPose Score: ', pose.score)
             for label, keypoint in pose.keypoints.items():
-                print('  %-20s x=%-4d y=%-4d score=%.1f' %
-                    (label, keypoint.point[0], keypoint.point[1], keypoint.score))
+                data[label.name] = {'x': str(
+                    keypoint.point[0]/640), 'y': str(keypoint.point[1]/480), 'score': str(keypoint.score)}
+
+            sio.emit('data', data)
 
         # Display the resulting frame
         cv2.imshow('frame', frame)
@@ -88,6 +116,7 @@ def main():
 
     cap.release()
     cv2.destroyAllWindows()
+
 
 def append_objs_to_img(cv2_im, inference_size, objs, labels):
     height, width, channels = cv2_im.shape
@@ -104,6 +133,7 @@ def append_objs_to_img(cv2_im, inference_size, objs, labels):
         cv2_im = cv2.putText(cv2_im, label, (x0, y0+30),
                              cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
     return cv2_im
+
 
 if __name__ == '__main__':
     main()
